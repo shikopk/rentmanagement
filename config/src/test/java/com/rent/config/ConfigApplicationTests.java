4b27c95896b86c008bbe2a5d6791778560f75bbb
Prateek Khandelwal <prateek.khandelwal@here.com>
2021-09-08 09:36:49 +0530

    Microservices

---
+++ config/src/test/java/com/rent/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
